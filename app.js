43c408d5c9ddf3482a8dc48d83bf2c2874fdbe11
Josep Llodrà <jlg.hrtc@gmail.com>
2015-03-03 19:08:19 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+$(document).on("pagecreate", "#pageone", function(){
+  console.log("PAGECREATE pageone");
+});
+
+$(document).on("pagecreate", "#pagetwo", function(){
+  console.log("PAGECREATE pagetwo");
+});
+
+$(document).on("pagecontainerbeforeshow", function(){
+  console.log("PAGECONTAINERBEFORESHOW change page");
+});
+
+$(document).on("pagecontainershow", function(){
+  console.log("PAGECONTAINERSHOW change page");
+});
